cc0ce8780012bf4fe82038ae8bb73bf32e4fc6e7
lyuka <niouja@gmail.com>
2014-08-15 22:28:53 +0800

    add binarysearch implemented with recursive

--- mysearch.py
+++ mysearch.py
@@ -15,7 +15,24 @@ def binarySearch( theValues, target ):
 
     return False
 
+# Performs a recursive binary search on a sorted sequence.
+def recBinarySearch( theSeq, target, first, last ):
+    if first > last:
+        return False
+    else:
+        mid = ( last + first ) // 2
+        if theSeq[mid] == target:
+            return True
+        elif target < theSeq[mid]:
+            return recBinarySearch( theSeq, target, first, mid - 1)
+        else:
+            return recBinarySearch( theSeq, target, mid + 1, last )
+
 if __name__ == '__main__':
     l_value = [2, 4, 5, 10, 13, 18, 23, 29, 31, 51, 64]
     t_value = 10
+    print 'binary search using loop: '
     print binarySearch(l_value, t_value)
+
+    print 'bianry search using recursive: '
+    print recBinarySearch( l_value, t_value, 0, len(l_value) )
